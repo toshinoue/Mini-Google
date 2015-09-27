10d93456ccae674f63e55fe66fa075ea76cf2445
toshinoue <kleber_yuji@hotmail.com>
2015-09-27 19:16:48 -0300

    Update miniGoogle.h

--- miniGoogle.h
+++ miniGoogle.h
@@ -1,8 +1,35 @@
 #ifndef MINI_GOOGLE_H
 #define MINI_GOOGLE_H
 
-#define TAM 50
-#define N 100
+#define TAM 51
+#define N 101
+#define TRUE 1
+#define FALSE 0
+
+typedef struct tipo_item ITEM;
+typedef struct node NO;
+typedef struct lista LISTA;
+typedef int boolean;
+
+LISTA *criar_lista();
+ITEM *criar_item(/*parametros para criar site*/);
+NO *criar_no(ITEM *item);
+
+void apagar_lista(LISTA **ptr);
+void esvazia_lista(NO *ptr);
+
+boolean vazia(LISTA *lista);
+int tamanho(LISTA *lista);
+void imprime_lista(LISTA *lista);
+
+boolean insere_site(LISTA *lista, NO *p);
+boolean insere_chave(LISTA *lista, int codigo);
+boolean remove_site(LISTA *lista, int codigo);
+
+void atualiza_relevancia(LISTA *lista, int codigo);
+
+void busca_chave(LISTA *lista, char *chave);
+void sugestao_site(LISTA *lista, char *chave);
 
 
 #endif
