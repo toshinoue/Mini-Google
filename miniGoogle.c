37ff091db1e6b3ebf5a6f73f87cab3fbdf79dd64
HenriqueAnraki <henrique_anraki@outlook.com>
2015-09-29 00:17:55 -0300

    ideia incial

--- miniGoogle.c
+++ miniGoogle.c
@@ -261,4 +261,18 @@ void busca_palavra(LISTA *lista, char *chave){
 
 }
 
-void sugestao_site(LISTA *lista, char *chave);
+void sugestao_site(LISTA *lista, char *chave){
+    LISTA_SEQ *palavras = criar_lista();
+    LISTA *aux = criar_lista_seq();
+
+    NO *p = lista->cabeca->proximo;
+    while(p != NULL){
+        if(busca_chave(p->item->palavras, chave, 0, tamanho_seq(p->item->palavras)-1)){
+            //if(busca_chave(palavras, ))
+            //inserir palavras na lista
+        }
+        p = p->proximo;
+    }
+
+
+}
