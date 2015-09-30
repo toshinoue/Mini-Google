c77c873ecf3c1528accdcc9b9e3e87175aa687dd
toshinoue <kleber_yuji@hotmail.com>
2015-09-30 15:09:44 -0300

    Update lista_seq_din.c

--- lista_seq_din.c
+++ lista_seq_din.c
@@ -8,14 +8,13 @@ struct item_seq{
 };
 
 struct lista_seq{
-    //ITEM_SEQ array[10];
     ITEM_SEQ *array;
     int tamanho;
 };
 
 
 
-void troca(ITEM_SEQ *array, int i, int j){
+void troca_seq(ITEM_SEQ *array, int i, int j){
     CHAVE aux[51];
     if(array != NULL){
         strcpy(aux, array[i].chave);
@@ -24,14 +23,14 @@ void troca(ITEM_SEQ *array, int i, int j){
     }
 }
 
-void insertion_sort(LISTA_SEQ *lista){
+void insertion_sort_seq(LISTA_SEQ *lista){
     int i, j;
 
     if(lista != NULL && lista->tamanho > 0){
         for (i = 1; i < lista->tamanho; i++) {
             j = i;
             while (j > 0 && strcmp(lista->array[j-1].chave, lista->array[j].chave) > 0 ) {
-                troca(lista->array, j, j-1);
+                troca_seq(lista->array, j, j-1);
                 j--;
             }
         }
@@ -48,7 +47,7 @@ LISTA_SEQ *criar_lista_seq(){
     return lista;
 }
 
-void liberar_lista(LISTA_SEQ **lista){
+void liberar_lista_seq(LISTA_SEQ **lista){
     if(lista != NULL && (*lista) != NULL){
         if((*lista)->array != NULL){
             free((*lista)->array);
@@ -59,17 +58,17 @@ void liberar_lista(LISTA_SEQ **lista){
     }
 }
 
-void inserir_ordenado(LISTA_SEQ *lista, CHAVE *chave){
+void inserir_ordenado_seq(LISTA_SEQ *lista, CHAVE *chave){
     if(lista != NULL && strlen(chave) <= TAM_PALAVRA-1){
         lista->tamanho++;
         lista->array = (ITEM_SEQ*)realloc(lista->array, sizeof(ITEM_SEQ)*lista->tamanho);
         strcpy(lista->array[lista->tamanho-1].chave, chave);
 
-        insertion_sort(lista);
+        insertion_sort_seq(lista);
     }
 }
 
-int listaVazia(LISTA_SEQ *lista){
+int listaVazia_seq(LISTA_SEQ *lista){
     if(lista != NULL && lista->array != NULL && lista->tamanho > 0){
         return 0;
     } else {
@@ -86,16 +85,16 @@ void imprime_lista_seq(LISTA_SEQ *lista){
     }
 }
 
-int busca_chave(LISTA_SEQ *lista, CHAVE *chave, int ini, int fim){
-    if(!listaVazia(lista)){
+int busca_chave_seq(LISTA_SEQ *lista, CHAVE *chave, int ini, int fim){
+    if(!listaVazia_seq(lista)){
         if(ini <= fim){
             int centro = (int)((fim+ini)/ 2.0);
 
             if(strcmp(lista->array[centro].chave, chave) == 0) {
                 return 1;
             }
-            if(strcmp(lista->array[centro].chave, chave) < 0) return busca_chave(lista, chave, centro+1, fim);
-            else return busca_chave(lista, chave, ini, centro-1);
+            if(strcmp(lista->array[centro].chave, chave) < 0) return busca_chave_seq(lista, chave, centro+1, fim);
+            else return busca_chave_seq(lista, chave, ini, centro-1);
         }
     }
     return 0;
@@ -105,13 +104,13 @@ int tamanho_seq(LISTA_SEQ *lista){
     return (lista->tamanho);
 }
 
-int transfere(LISTA_SEQ *lista, LISTA_SEQ *secundaria){
+int transfere_seq(LISTA_SEQ *lista, LISTA_SEQ *secundaria){
     int i;
 
     if(secundaria != NULL && lista != NULL){
         for(i = 0; i < secundaria->tamanho; i++){
-            if(listaVazia(lista) || !(busca_chave(lista, secundaria->array[i].chave, 0, lista->tamanho-1))){
-                inserir_ordenado(lista, secundaria->array[i].chave);
+            if(listaVazia_seq(lista) || !(busca_chave_seq(lista, secundaria->array[i].chave, 0, lista->tamanho-1))){
+                inserir_ordenado_seq(lista, secundaria->array[i].chave);
             }
         }
         return 1;
@@ -119,7 +118,7 @@ int transfere(LISTA_SEQ *lista, LISTA_SEQ *secundaria){
     return 0;
 }
 
-CHAVE *remove_fim(LISTA_SEQ *lista){
+CHAVE *remove_fim_seq(LISTA_SEQ *lista){
     CHAVE *ret = (char *)malloc(sizeof(char) * strlen(lista->array[lista->tamanho - 1].chave)+1);
     lista->tamanho--;
     strcpy(ret, lista->array[lista->tamanho].chave);
@@ -127,7 +126,7 @@ CHAVE *remove_fim(LISTA_SEQ *lista){
     return ret;
 }
 
-CHAVE *retorna_chave_posicao(LISTA_SEQ *lista, int pos){
+CHAVE *retorna_chave_posicao_seq(LISTA_SEQ *lista, int pos){
     if(pos < lista->tamanho && pos >= 0){
         CHAVE *ret = (char *)malloc(sizeof(char) * strlen(lista->array[pos].chave)+1);
         strcpy(ret, lista->array[pos].chave);
